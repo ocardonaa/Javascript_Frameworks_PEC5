79fbb9752e1f7c398d2252979c8151a65763fe5a
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-05-14 11:50:19 +0200

    ej2

--- PEC5_Ej_Prac/ecommerce/src/app/app.module.ts
+++ PEC5_Ej_Prac/ecommerce/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleItemComponent } from './article-item/article-item.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { ArticleNewTemplateComponent } from './article-new-template/article-new-template.component';
+import { ArticleNewReactiveComponent } from './article-new-reactive/article-new-reactive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ArticleItemComponent,
-    ArticleListComponent
+    ArticleListComponent,
+    NavbarComponent,
+    ArticleNewTemplateComponent,
+    ArticleNewReactiveComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
   providers: [
     provideClientHydration()
